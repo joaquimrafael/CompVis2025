2de252880091f6628551bd4d6f4ebf4b94f0c74b
Lucas <ltrebacchetti@gmail.com>
2025-09-14 21:48:07 -0300

    Screen centered and converting to Gray

--- main.c
+++ main.c
@@ -58,7 +58,6 @@ static MyImage g_image = {
 static bool MyWindow_initialize(MyWindow *window, const char *title, int width, int height, SDL_WindowFlags window_flags);
 static void MyWindow_destroy(MyWindow *window);
 static void MyImage_destroy(MyImage *image);
-static void load_rgba32(const char *filename, SDL_Renderer *renderer, MyImage *output_image);
 
 // Function implementations
 
@@ -112,67 +111,6 @@ void MyImage_destroy(MyImage *image)
   image->rect.x = image->rect.y = image->rect.w = image->rect.h = 0.0f;
 
   SDL_Log("<<< MyImage_destroy()");
-}
-
-void load_rgba32(const char *filename, SDL_Renderer *renderer, MyImage *output_image)
-{
-  SDL_Log(">>> load_rgba32(\"%s\")", filename);
-
-  if (!filename)
-  {
-    SDL_Log("\t*** Erro: Nome do arquivo inválido (filename == NULL).");
-    SDL_Log("<<< load_rgba32(\"%s\")", filename);
-    return;
-  }
-
-  if (!renderer)
-  {
-    SDL_Log("\t*** Erro: Renderer inválido (renderer == NULL).");
-    SDL_Log("<<< load_rgba32(\"%s\")", filename);
-    return;
-  }
-
-  if (!output_image)
-  {
-    SDL_Log("\t*** Erro: Imagem de saída inválida (output_image == NULL).");
-    SDL_Log("<<< load_rgba32(\"%s\")", filename);
-    return;
-  }
-
-  MyImage_destroy(output_image);
-
-  SDL_Log("\tCarregando imagem \"%s\" em uma superfície...", filename);
-  SDL_Surface *surface = IMG_Load(filename);
-  if (!surface)
-  {
-    SDL_Log("\t*** Erro ao carregar a imagem: %s", SDL_GetError());
-    SDL_Log("<<< load_rgba32(\"%s\")", filename);
-    return;
-  }
-
-  SDL_Log("\tConvertendo superfície para formato RGBA32...");
-  output_image->surface = SDL_ConvertSurface(surface, SDL_PIXELFORMAT_RGBA32);
-  SDL_DestroySurface(surface);
-  if (!output_image->surface)
-  {
-    SDL_Log("\t*** Erro ao converter superfície para formato RGBA32: %s", SDL_GetError());
-    SDL_Log("<<< load_rgba32(\"%s\")", filename);
-    return;
-  }
-
-  SDL_Log("\tCriando textura a partir da superfície...");
-  output_image->texture = SDL_CreateTextureFromSurface(renderer, output_image->surface);
-  if (!output_image->texture)
-  {
-    SDL_Log("\t*** Erro ao criar textura: %s", SDL_GetError());
-    SDL_Log("<<< load_rgba32(\"%s\")", filename);
-    return;
-  }
-
-  SDL_Log("\tObtendo dimensões da textura...");
-  SDL_GetTextureSize(output_image->texture, &output_image->rect.w, &output_image->rect.h);
-
-  SDL_Log("<<< load_rgba32(\"%s\")", filename);
 }
 
 static SDL_AppResult initialize(void)
@@ -255,6 +193,142 @@ static void loop(void)
 }
 
 //------------------------------------------------------------------------------
+void createWindow()
+{
+    int imageWidth = (int)g_image.rect.w;
+    int imageHeight = (int)g_image.rect.h;
+    if (imageWidth > DEFAULT_WINDOW_WIDTH || imageHeight > DEFAULT_WINDOW_HEIGHT)
+    {
+        int top = 0;
+        int left = 0;
+        SDL_GetWindowBordersSize(g_window.window, &top, &left, NULL, NULL);
+
+        SDL_Log("Redefinindo dimensões da janela, de (%d, %d) para (%d, %d), e alterando a posição para (%d, %d).",
+        DEFAULT_WINDOW_WIDTH, DEFAULT_WINDOW_HEIGHT, imageWidth, imageHeight, left, top);
+
+        SDL_SetWindowSize(g_window.window, imageWidth, imageHeight);
+        SDL_SetWindowPosition(g_window.window, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED);
+
+        SDL_SyncWindow(g_window.window);
+    }
+}
+
+bool isGrayScale(SDL_Surface *surface)
+{
+  SDL_Log("<<< isGrayScale()");
+  SDL_LockSurface(surface);
+  Uint32 *pixel = (Uint32*)surface->pixels;
+  int size = surface->w*surface->h;
+  const SDL_PixelFormatDetails *format = SDL_GetPixelFormatDetails(surface->format);
+
+  for(int i=0;i<size;i++)
+  {
+    Uint8 r,g,b,a;
+    SDL_GetRGBA(pixel[i], format,NULL,&r,&g,&b,&a);
+    if(!(r==g && g==b)){
+      SDL_UnlockSurface(surface);
+      return(false);
+    }
+  }
+
+  SDL_Log("<<< isGrayScale()");
+  SDL_UnlockSurface(surface);
+  return(true);
+}
+
+void convertToGray(SDL_Surface *surface)
+{
+    SDL_Log("<<< convertToGray()");
+    if (!surface) return;
+    SDL_LockSurface(surface);
+    Uint32 *pixel = (Uint32*)surface->pixels;
+    int size = surface->w*surface->h;
+
+    const SDL_PixelFormatDetails *format = SDL_GetPixelFormatDetails(surface->format);
+
+    for (int i=0;i<size;i++)
+    {
+        Uint8 r,g,b,a;
+        SDL_GetRGBA(pixel[i],format,NULL,&r,&g,&b,&a);
+        double y = 0.2125*(double)+0.7154*(double)g+0.0721*(double)b;
+        Uint8 gray = (Uint8)y;
+
+        pixel[i] = SDL_MapRGBA(format,NULL,gray,gray,gray,a);
+    }
+
+    SDL_UnlockSurface(surface);
+    SDL_Log(">>> convertToGray()");
+}
+
+
+void loadImage(const char *filename, SDL_Renderer *renderer, MyImage *output_image)
+{
+  SDL_Log(">>> loadImage(\"%s\")", filename);
+
+  if (!filename)
+  {
+    SDL_Log("\t*** Erro: Nome do arquivo inválido (filename == NULL).");
+    SDL_Log("<<< loadImage(\"%s\")", filename);
+    return;
+  }
+
+  if (!renderer)
+  {
+    SDL_Log("\t*** Erro: Renderer inválido (renderer == NULL).");
+    SDL_Log("<<< loadImage(\"%s\")", filename);
+    return;
+  }
+
+  if (!output_image)
+  {
+    SDL_Log("\t*** Erro: Imagem de saída inválida (output_image == NULL).");
+    SDL_Log("<<< loadImage(\"%s\")", filename);
+    return;
+  }
+
+  MyImage_destroy(output_image);
+
+  SDL_Log("\tCarregando imagem \"%s\" em uma superfície...", filename);
+  SDL_Surface *surface = IMG_Load(filename);
+  if (!surface)
+  {
+    SDL_Log("\t*** Erro ao carregar a imagem: %s", SDL_GetError());
+    SDL_Log("<<< loadImage(\"%s\")", filename);
+    return;
+  }
+
+  SDL_Log("\tConvertendo superfície para formato RGBA32...");
+  output_image->surface = SDL_ConvertSurface(surface, SDL_PIXELFORMAT_RGBA32);
+  SDL_DestroySurface(surface);
+  if (!output_image->surface)
+  {
+    SDL_Log("\t*** Erro ao converter superfície para formato RGBA32: %s", SDL_GetError());
+    SDL_Log("<<< load_rgba32(\"%s\")", filename);
+    return;
+  }
+
+  bool isGray = isGrayScale(output_image->surface);
+  isGrayScale(output_image->surface) ? SDL_Log("\tÉ cinza") : SDL_Log("\tÉ colorida");
+
+  if(!isGray){
+    convertToGray(output_image->surface);
+  }
+
+
+  SDL_Log("\tCriando textura a partir da superfície...");
+  output_image->texture = SDL_CreateTextureFromSurface(renderer, output_image->surface);
+  if (!output_image->texture)
+  {
+    SDL_Log("\t*** Erro ao criar textura: %s", SDL_GetError());
+    SDL_Log("<<< load_rgba32(\"%s\")", filename);
+    return;
+  }
+
+  SDL_Log("\tObtendo dimensões da textura...");
+  SDL_GetTextureSize(output_image->texture, &output_image->rect.w, &output_image->rect.h);
+
+  SDL_Log("<<< load_rgba32(\"%s\")", filename);
+}
 
 int main(int argc, char *argv[])
 {
@@ -270,24 +344,9 @@ int main(int argc, char *argv[])
 
     IMAGE_FILENAME = argv[1];
 
-    load_rgba32(IMAGE_FILENAME, g_window.renderer, &g_image);
+    loadImage(IMAGE_FILENAME, g_window.renderer, &g_image);
 
-    int imageWidth = (int)g_image.rect.w;
-    int imageHeight = (int)g_image.rect.h;
-    if (imageWidth > DEFAULT_WINDOW_WIDTH || imageHeight > DEFAULT_WINDOW_HEIGHT)
-    {
-        int top = 0;
-        int left = 0;
-        SDL_GetWindowBordersSize(g_window.window, &top, &left, NULL, NULL);
-
-        SDL_Log("Redefinindo dimensões da janela, de (%d, %d) para (%d, %d), e alterando a posição para (%d, %d).",
-        DEFAULT_WINDOW_WIDTH, DEFAULT_WINDOW_HEIGHT, imageWidth, imageHeight, left, top);
-
-        SDL_SetWindowSize(g_window.window, imageWidth, imageHeight);
-        SDL_SetWindowPosition(g_window.window, left, top);
-
-        SDL_SyncWindow(g_window.window);
-    }
+    createWindow();
 
     loop();
 
